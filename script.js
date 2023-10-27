d2e3e372c7da077109810bf3416b8006319ee2fa
mallikapalani <149163689+mallikapalani@users.noreply.github.com>
2023-10-27 05:36:13 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+let signup = document.querySelector(".signup");
+let login = document.querySelector(".login");
+let slider = document.querySelector(".slider");
+let formSection = document.querySelector(".form-section");
+ 
+signup.addEventListener("click", () => {
+    slider.classList.add("moveslider");
+    formSection.classList.add("form-section-move");
+});
+ 
+login.addEventListener("click", () => {
+    slider.classList.remove("moveslider");
+    formSection.classList.remove("form-section-move");
+});
+function mymessage() {
+    alert("SUCCESSFULLY LOGGED IN");
+  }
+  function bmessage() {
+    alert("YOUR TICEKET IS BOOKED");
+  }
